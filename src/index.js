cb7ae0099e0a6d80f90f0f2b69dabb7c14c5e1b5
Abubakar <athman.abu@gmail.com>
2020-07-02 00:08:03 +0300

    setup context

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./context";
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { ProductConsumer } from "../context";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from home page</h1>
-    </div>
+    <>
+      <ProductConsumer>{(value) => <h1>{value}</h1>}</ProductConsumer>
+    </>
   );
 }
