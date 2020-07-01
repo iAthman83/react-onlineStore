6591338a9b97ae78709410666ccacf042e87b84f
Abubakar <athman.abu@gmail.com>
2020-07-01 23:37:44 +0300

    create pages and links

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Cart from "./pages/CartPage";
+import Contact from "./pages/ContactPage";
+import Default from "./pages/DefaultPage";
+import Products from "./pages/ProductsPage";
+import SinglePoduct from "./pages/SingleProductPage";
+
+import { Route, Switch } from "react-router-dom";
+
 function App() {
-  return <h1>Hello form mtoosh</h1>;
+  return (
+    <>
+      {/* navbar, sidebar, cart, footer */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" component={SinglePoduct} />
+        <Route component={Default} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>Hello from product page</h1>
+    </div>
+  );
+}
