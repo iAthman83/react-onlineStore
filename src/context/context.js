c9e9ed41487d1d652104b8d403b9446b032a3742
Abubakar <athman.abu@gmail.com>
2020-07-02 00:22:01 +0300

    context api

--- src/context/context.js
+++ src/context/context.js
@@ -5,9 +5,50 @@ const ProductContext = React.createContext();
 // provider and cosumer
 
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0,
+  };
+  // handle sidebar
+  handleSidebar = () => {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen,
+    });
+  };
+
+  // handle cart
+  handleCart = () => {
+    this.setState({
+      cartOpen: !this.state.cartOpen,
+    });
+  };
+
+  // close cart
+  closeCart = () => {
+    this.setState({
+      cartOpen: false,
+    });
+  };
+
+  // open cart
+  openCart = () => {
+    this.setState({
+      cartOpen: true,
+    });
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,12 @@ import { ProductConsumer } from "../context";
 export default function HomePage() {
   return (
     <>
-      <ProductConsumer>{(value) => <h1>{value}</h1>}</ProductConsumer>
+      <ProductConsumer>
+        {(value) => {
+          console.log(value);
+          return <h1>Hello from Home page</h1>;
+        }}
+      </ProductConsumer>
     </>
   );
 }
