9e775b5ec5014e70d449fe721816e59e34a1096e
Abubakar <athman.abu@gmail.com>
2020-07-01 23:55:15 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,21 +12,33 @@ import SinglePoduct from "./pages/SingleProductPage";
 
 import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      {/* navbar, sidebar, cart, footer */}
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/products" exact component={Products} />
-        <Route path="/products/:id" component={SinglePoduct} />
-        <Route component={Default} />
-      </Switch>
-    </>
-  );
+import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
+import SideCart from "./components/SideCart";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        {/* navbar, sidebar, cart, footer */}
+        <Navbar />
+        <SideBar />
+        <SideCart />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id" component={SinglePoduct} />
+          <Route component={Default} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
