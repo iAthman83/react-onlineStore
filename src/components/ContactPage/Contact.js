dead4c2d2cd303de63b84b31b956dfc52c3748a5
Abubakar <athman.abu@gmail.com>
2020-07-05 00:42:21 +0300

    contact page

---
+++ src/components/ContactPage/Contact.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Title from "../Title";
+
+export default function Contact() {
+  return (
+    <section className="py-5">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-6 my-3">
+          <Title title="contact us" />
+          <form className="mt-5">
+            {/* first form group */}
+            <div className="form-group">
+              <input
+                type="text"
+                name="firstName"
+                className="form-control"
+                placeholder="Abu Athman"
+              />
+            </div>
+            {/* email form group */}
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="abc@example.com"
+              />
+            </div>
+            {/* subject form group */}
+            <div className="form-group">
+              <input
+                type="text"
+                name="subject"
+                className="form-control"
+                placeholder="important!!!"
+              />
+            </div>
+            {/* message */}
+            <div className="form-group">
+              <textarea
+                name="message"
+                className="form-control"
+                rows="10"
+                placeholder="hello friend"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
