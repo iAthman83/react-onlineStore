2d2e3dd5089446a439d0dd6217954a33e12e4d87
Abubakar <athman.abu@gmail.com>
2020-07-05 10:51:57 +0300

    footer setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { ProductConsumer } from "../context";
+import styled from "styled-components";
 
 export default function Footer() {
-  return <>Hello from footer</>;
+  return (
+    <ProductConsumer>
+      {(value) => {
+        return (
+          <FooterWrapper>
+            <div className="container py-3">
+              <div className="row">
+                <div className="col-md-6">
+                  <p className="text-capitalize">
+                    copyright &copy; woosh {new Date().getFullYear()}. all
+                    rights reserved
+                  </p>
+                </div>
+                <div className="col-md-6 d-flex justify-content-around">
+                  {value.socialIcons.map((item) => {
+                    return (
+                      <a href={item.url} target="_blank" key={item.id}>
+                        {item.icon}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </FooterWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 }
+
+const FooterWrapper = styled.footer`
+  background: var(--darkGrey);
+  color: var(--mainWhite);
+  .icon {
+    font-size: 1.5rem;
+    color: var(--mainWhite);
+    transition: var(--mainTransition);
+  }
+  .icon:hover {
+    color: var(--primaryColor);
+    cursor: pointer;
+  }
+`;
