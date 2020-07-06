0356d116c7eb04c8df4733bf6f55099e3a28122b
Abubakar <athman.abu@gmail.com>
2020-07-06 23:07:14 +0300

    setup featured products

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Product from "../Product";
+import Title from "../Title";
+import { ProductConsumer } from "../../context";
 
 export default function Featured() {
-  return <div>hello from featured</div>;
+  return (
+    <section className="py-5">
+      <div className="container">
+        {/* title */}
+        <Title title="featured products" center="true" />
+
+        {/* products */}
+        <div className="row">
+          <ProductConsumer>
+            {(value) => {
+              const { featuredProducts } = value;
+              return featuredProducts.map((product) => (
+                <Product key={product.id} product={product} />
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+        <div className="row mt-5">
+          <div className="col text-center">
+            <Link to="products" className="main-link">
+              our products
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export default function Title({ title, center }) {
   return (
-    <TitleWrapper className="row" cetner={center}>
+    <TitleWrapper className="row" center={center}>
       <div className="col">
         <h2 className="text-title">{title}</h2>
         <div className="title-underline"></div>
