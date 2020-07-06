6bad024016dfcf99a9ee4d4b3f79d6cc3f4a0e1c
Abubakar <athman.abu@gmail.com>
2020-07-06 23:46:21 +0300

    setup products page

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Products from "../components/ProductsPage/Products";
+import Hero from "../components/Hero";
+import productsBcg from "../images/productsBcg.jpeg";
 
 export default function ProductsPage() {
   return (
-    <div>
-      <h1>Hello from product page</h1>
-    </div>
+    <>
+      <Hero img={productsBcg} />
+      <Products />
+    </>
   );
 }
